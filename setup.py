3918922ad0739f9fcab4eaae83b38ffb28212f8b
Vaibhav Kulshrestha <25487984+vaibhav-k@users.noreply.github.com>
2025-10-20 13:39:34 -0500

    Update setup.py by adding GitHub URL and updating requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Author:
     Vaibhav Kulshrestha
 
 Date:
-    2025-10-18
+    2025-10-20
 """
 
 # Import necessary modules
@@ -13,9 +13,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="model_explain",
-    version="0.3.0",
+    version="0.3.1",
     packages=find_packages(),
     install_requires=[
+        "torch",
         "plotly",
         "fairlearn",
         "dice-ml",
@@ -33,8 +34,13 @@ setup(
     entry_points={"console_scripts": ["explain-cli=model_explain.cli:main"]},
     author="Vaibhav Kulshrestha",
     author_email="vaibhav1kulshrestha@gmail.com",
-    description="A model explainability toolkit with SHAP, LIME, scoring, summaries, and GUI sandbox",
-    long_description=open("README.md").read(),
+    description="A package for interpreting and explaining machine learning models. It provides a unified interface for popular explanation techniques, including LIME, SHAP, and Grad-CAM and supports a wide range of models and data types.",
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    python_requires=">=3.7",
+    python_requires=">=3.8",
+    url="https://github.com/vaibhav-k/model_explain",  # GitHub repository
+    project_urls={
+        "Source": "https://github.com/vaibhav-k/model_explain",
+        "Tracker": "https://github.com/vaibhav-k/model_explain/issues",
+    },
 )
